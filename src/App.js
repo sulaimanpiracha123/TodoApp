f58b13c0d12364dc0fbc8848afbb77833e95dbe4
Sulaiman <sulaiman@Usmans-MacBook-Pro.local>
2022-05-26 15:40:36 +0500

    ToDoApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ToDoList from "./TodoList";
+const App =()=>{
+  const [inputList,SetInputList]=useState("")
+  const [Items,setItems]= useState([])
+  const itemEvent = (event)=>
+  {
+    SetInputList(event.target.value)
+  }
+  const ListofItems = ()=>{
+    setItems((oldItems)=>[...oldItems,inputList])
+    SetInputList("")
+  }
+  const deleteItem =(id)=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElem,index)=>{
+        return index !== id;
+      })
+    })
+
+
+   }
+  return(
+    <>
+    <div className="Main_div">
+      <div className="Center_div">
+        <br/>
+        <h1>Todo List</h1>
+        <br/>
+        <input type="text" placeholder="Add a Items" value={inputList} onChange={itemEvent}/>
+        <button onClick={ListofItems}>+</button>
+        <ol>
+        {/* <li>{inputList}</li> */}
+        {Items.map((Itemsval,index)=>{
+         return <ToDoList
+         key={index}
+         id={index}
+         text={Itemsval}
+         onSelect={deleteItem}/>
+        })}
+
+
+        </ol>
+      </div>
     </div>
-  );
-}
 
-export default App;
+    </>
+  )
+  
+}
+export default App
---
+++ src/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+const ToDoList = (props)=>{
+    
+    return(
+    <>
+    <div className="todo_style">
+
+     <ol>
+    
+        <li> <i className="fa fa-times" aria-hidden="true" onClick={()=>{
+         props.onSelect(props.id)
+
+     }}/> {props.text}
+    </li>
+     </ol>
+        
+    </div>
+    
+    </>
+    );
+
+}
+export default ToDoList 
